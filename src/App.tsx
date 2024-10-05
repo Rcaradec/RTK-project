de708aecfb35cd602e314a0ad3ff2d22ee64d5d0
Rcaradec <remi.caradec@matou-technologies.fr>
2024-10-05 23:41:42 +0200

    dependancies

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { Typography } from "@mui/material";
+
+function App() {
+  return (
+    <>
+      <Typography variant="h1" gutterBottom>
+        Redux / RTK initial Commit
+      </Typography>
+    </>
+  );
+}
+
+export default App;
